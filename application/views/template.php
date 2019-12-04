7a9a3529d1b0adbd68147a6ca1e106c2a71e164c
yughoz <alvianyusuf22@gmail.com>
2019-12-04 16:18:14 +0800

    add blasting

--- application/views/template.php
+++ application/views/template.php
@@ -337,7 +337,7 @@
         <!-- page script -->
         <script>
             $(function () {
-                $('.select2').select2()
+                // $('.select2').select2()
                 $('#example1').DataTable()
                 $('#example2').DataTable({
                     'paging'      : true,
